8c400482fcebfaec7df4636d08357328cee488d1
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2025-03-24 10:26:04 +0800

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+
+const posts = document.getElementById("posts");
+
+// const post = params.get("post");
+
+async function viewPost(post) {
+    window.location.href = `post.html?${post}`;
+}
